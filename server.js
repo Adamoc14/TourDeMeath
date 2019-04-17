7f12a76171e3d5a30d09f7c65949209df63b3061
adamoc <oceallaighadam96@gmail.com>
2019-04-17 14:22:29 +0100

    Built for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static("public"));
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+app.listen(port);
